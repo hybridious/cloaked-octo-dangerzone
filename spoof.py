3e46c9889cb33c1a091c06612669597172a9a305
thezilla <321homerepair@gmail.com>
2014-04-15 16:42:55 -0400

    Create spoof.py
    Untested.

---
+++ spoof.py
@@ -0,0 +1,20 @@
+import random
+import zipfile
+
+"""
+My poor attempt @ porting a msf exploit to python.
+
+Run with:
+    python spoof.py file_to_spoof new_spoofed_filename
+"""
+file = sys.argv[0]
+new_file = sys.argv[1]
+
+def spoof_file(file, newfile):
+    new_filename = ''.join(random.choice('0123456789abcdefghijklmnopqurstuvwxyz') for i in range(16)) + ".exe"
+    zip = zipfile.ZipFile('new_filename', 'w')
+    zip.write(new_filename)
+    zip.close()
+    print "Created file: " + new_filename
+
+spoof_file()
